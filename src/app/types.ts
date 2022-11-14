cccbe52b57e839d4b8ae1247f127b30eb6c2414e
SVA2021 <sofronov.vit@gmail.com>
2022-11-14 12:25:45 +0400

    add types and consts

---
+++ src/app/types.ts
@@ -0,0 +1,40 @@
+export interface AuthI {
+    email: string
+    password: string
+    personal_data_access: boolean
+}
+
+export interface AuthResponseI {
+    msg: string
+    data: {
+        msg: string
+        data: {
+            access_token: string
+            token_type: "Bearer"
+            expires_at: number
+        }
+    }
+}
+
+export interface DeviceI {
+    id: number
+    name: string
+    last_active: number | string
+}
+
+export const URL_LOGIN = 'https://core.nekta.cloud/api/auth/login';
+export const URL_DEVICES = 'https://core.nekta.cloud/api/device/metering_devices';
+export const TEST_EMAIL = 'demo@nekta.tech';
+export const TEST_PWD = 'qwertyqwerty';
+export const TEST_REQUEST = {
+    "page": 1,
+    "last_page": 0,
+    "sort_field": "id",
+    "sort": "desc",
+    "search_string": null,
+    "device_state": "all",
+    "is_archived": false,
+    "paginate": true,
+    "append_fields": ["active_polling", "attributes", "tied_point"],
+    "per_page": 10
+};
