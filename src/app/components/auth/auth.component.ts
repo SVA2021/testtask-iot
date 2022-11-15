7ac9c6129d1a56a467f2c7a983ddb40c2eaf148c
SVA2021 <sofronov.vit@gmail.com>
2022-11-15 12:51:21 +0400

    add auth component

--- src/app/components/auth/auth.component.ts
+++ src/app/components/auth/auth.component.ts
@@ -1,4 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {FormControl, FormGroup, Validators} from '@angular/forms';
+import {Router} from '@angular/router';
+import {AuthService} from 'src/app/services/auth.service';
+import {AuthI} from 'src/app/types';
 
 @Component({
   selector: 'app-auth',
@@ -7,9 +11,46 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AuthComponent implements OnInit {
 
-  constructor() { }
+  form: FormGroup
+  isSubmitted = false
+
+  constructor(
+    public auth: AuthService,
+    private router: Router,
+  ) { }
 
   ngOnInit(): void {
+
+    this.form = new FormGroup({
+      email: new FormControl(null, [
+        Validators.required,
+        Validators.email
+      ]),
+      password: new FormControl(null, [
+        Validators.required,
+      ])
+    })
   }
 
+
+  submit() {
+    if (this.form.invalid) {
+      return
+    }
+
+    this.isSubmitted = true
+
+    const user: AuthI = {
+      email: this.form.value.email,
+      password: this.form.value.password,
+      personal_data_access: true
+    }
+
+    this.auth.login(user).subscribe(() => {
+      this.form.reset()
+      this.isSubmitted = false
+      this.router.navigate(['/table'])
+    })
+
+  }
 }
