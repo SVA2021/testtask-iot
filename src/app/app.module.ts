5296a034eea7949629a523e6a3d2543dfaad2974
SVA2021 <sofronov.vit@gmail.com>
2022-11-15 12:14:37 +0400

    add imports for app module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,33 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import {HttpClientModule} from '@angular/common/http';
+import {NgModule} from '@angular/core';
+import {ReactiveFormsModule} from '@angular/forms';
+import {BrowserModule} from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { AuthComponent } from './components/auth/auth.component';
-import { FooterComponent } from './components/footer/footer.component';
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+import {AuthComponent} from './components/auth/auth.component';
+import {ErrorComponent} from './components/error/error.component';
+import {FooterComponent} from './components/footer/footer.component';
+import {HomeComponent} from './components/home/home.component';
+import {TableComponent} from './components/table/table.component';
+import {AuthGuard} from './services/auth.guard';
 
 @NgModule({
   declarations: [
     AppComponent,
     AuthComponent,
-    FooterComponent
+    FooterComponent,
+    TableComponent,
+    ErrorComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    ReactiveFormsModule,
+    HttpClientModule,
+    AppRoutingModule,
   ],
-  providers: [],
+  providers: [AuthGuard],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
