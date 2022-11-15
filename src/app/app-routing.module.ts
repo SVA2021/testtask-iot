2fc03837fb495aa1ed80db741b6994f1e7f7db08
SVA2021 <sofronov.vit@gmail.com>
2022-11-15 11:54:19 +0400

    add base routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,30 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import {NgModule} from '@angular/core';
+import {PreloadAllModules, RouterModule, Routes} from '@angular/router';
+import {AuthComponent} from './components/auth/auth.component';
+import {ErrorComponent} from './components/error/error.component';
+import {HomeComponent} from './components/home/home.component';
+import {TableComponent} from './components/table/table.component';
+import {AuthGuard} from './services/auth.guard';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '', component: HomeComponent,
+  },
+  {
+    path: 'table', component: TableComponent, canActivate: [AuthGuard],
+  },
+  {
+    path: 'admin', component: AuthComponent,
+  },
+  {
+    path: '**', component: ErrorComponent,
+  }
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes, {
+    preloadingStrategy: PreloadAllModules,
+  })],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
